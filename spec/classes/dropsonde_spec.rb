eaf8ee38090a05ceab8c6d4ed2ac884dd03b0b2a
Ben Ford <ben.ford@puppetlabs.com>
2021-12-08 12:50:53 -0800

    Allow module to work with bundled gem
    A few things have changed now that the gem is bundled. We don't need to
    manage installation anymore, and it's scheduled with the built-in
    tkscheduler. Instead of managing a cron job, we'll just turn it on.
    
    Note that I didn't bother to update the Litmus tests because they'll all
    fail until the images are updated with the latest release, and I don't
    know how to choose the Puppetserver version to install anyway! 🤷

--- spec/classes/dropsonde_spec.rb
+++ spec/classes/dropsonde_spec.rb
@@ -8,18 +8,44 @@ describe 'dropsonde' do
       it { is_expected.to compile }
     end
 
-    context "on #{os} with enabled => false" do
+    context "on #{os} with legacy installation and enabled => false" do
       let(:facts) { os_facts }
       let(:params) { { enabled: false } }
 
+      it { is_expected.to contain_package('dropsonde').with_ensure('latest') }
       it { is_expected.to contain_cron('submit Puppet telemetry report').with_ensure('absent') }
     end
 
-    context "on #{os} with use_cron => false" do
+    context "on #{os} with legacy installation and enabled => true" do
       let(:facts) { os_facts }
-      let(:params) { { use_cron: false } }
+      let(:params) { { enabled: true } }
 
-      it { is_expected.not_to contain_cron('submit Puppet telemetry report') }
+      it { is_expected.to contain_package('dropsonde').with_ensure('latest') }
+      it { is_expected.to contain_cron('submit Puppet telemetry report').with_ensure('present') }
     end
+
+
+    context "on #{os} with vendored client and enabled => false" do
+      let(:facts) {
+        os_facts.merge({
+          :dropsonde => {:bundled => true, :version => '0.0.6'},
+        })
+      }
+      let(:params) { { enabled: false } }
+
+      it { is_expected.to contain_hocon_setting('submit Puppet telemetry report').with_value(false) }
+    end
+
+    context "on #{os} with vendored client and enabled => true" do
+      let(:facts) {
+        os_facts.merge({
+          :dropsonde => {:bundled => true, :version => '0.0.6'},
+        })
+      }
+      let(:params) { { enabled: true } }
+
+      it { is_expected.to contain_hocon_setting('submit Puppet telemetry report').with_value(true) }
+    end
+
   end
 end
